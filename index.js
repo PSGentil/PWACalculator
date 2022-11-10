92fde63988747fec7d286c02440dd8065876c1a9
PSGentil <108874841+PSGentil@users.noreply.github.com>
2022-11-10 20:18:40 -0300

    Tá ficando bonito

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const primo = document.getElementById('primo')
 const parimpar = document.getElementById('parimpar')
 const fatorial = document.getElementById('fatorial')
 const quadrado = document.getElementById('quadrado')
+const bhaskara = document.getElementById('bhaskara')
 
 const menu = document.getElementById('menu')
 const titulo = document.getElementById('titulo')
 
 const input1 = document.getElementById('input1')
 const input2 = document.getElementById('input2')
+const input3 = document.getElementById('input3')
 const resultButt = document.getElementById('resultButt')
 const result = document.getElementById('result')
 const resultText = document.getElementById('resultText')
@@ -17,8 +19,10 @@ titulo.onclick = () => {
     parimpar.style.display = 'none'
     fatorial.style.display = 'none'
     quadrado.style.display = 'none'
+    bhaskara.style.display = 'none'
     result.style.display = 'none'
     resultText.innerText = ''
+    resultText.style.position = 'relative'
 
     menu.style.display = 'block'
 }
@@ -106,6 +110,8 @@ function quadradar() {
     resultButt.onclick = () => {
         let val = Number(input1.value), val2 = Number(input2.value)
         resultText.innerText = ''
+        
+        resultText.style.position = 'static'
 
         if (val && val2) {
             for (let i = val; i <= val2; i++) {
@@ -113,6 +119,37 @@ function quadradar() {
             }
         } else {
             resultText.innerText = 'Valor inválido!'
+        }
+    }
+}
+function bhaskarar() {
+    visibilitar(menu, bhaskara)
+    bhaskara.appendChild(input1)
+    input1.style.display = 'inline-block'
+    input1.style.width = '20%'
+    bhaskara.appendChild(input2)
+    input2.style.display = 'inline-block'
+    input2.style.width = '20%'
+    bhaskara.appendChild(input3)
+    input3.style.display = 'inline-block'
+    input3.style.width = '20%'  
+
+    resultButt.onclick = () => {
+        
+        let a = Number(input1.value)
+        let b = Number(input2.value)
+        let c = Number(input3.value)
+
+        let delta = Number((b * b) - 4 * a * c)
+
+        if (delta > 0) {
+            let x1 = Number((-b + Math.sqrt(delta)) / (a * 2))
+            let x2 = Number((-b - Math.sqrt(delta)) / (a * 2))
+
+            resultText.innerText = `X1 = ${x1}  X2 = ${x2}`
+        }
+        else {
+            resultText.innerText = `Delta é negativo, então a equação não possui raizes reais`
         }
     }
 }
